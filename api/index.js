2fdd867b3bee704dcc0951e94305e81391fa10ff
leeyoungjik1 <eog91@naver.com>
2024-06-11 17:09:45 +0900

    몽고 아틀라스 연동

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const app = express();
+
+console.log("몽고아틀라스 주소: ", process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => console.log("데이터베이스 연결 성공 !!"))
+.catch(e => console.log(`데이터베이스 연결 실패 ${e}`))
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3001, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
